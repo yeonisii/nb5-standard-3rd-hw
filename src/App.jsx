4a358e704171af651d7b0c885b986adc40ab42e1
요니'^ <jjy606@naver.com>
2024-05-21 11:15:29 +0900

    과제 제출

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,20 @@ export default function App() {
   ];
 
   // TODO: filter를 사용하여 18세 이상의 학생들만 선택하세요.
-  const filteredStudents = students.filter(/* 여기에 코드 작성 */);
+  const filteredStudents = students.filter((students) => students.age >= 18);
 
   return (
     <div>
       <h1>학생 목록</h1>
       <ul>
-        {/* TODO: map을 사용해서 filteredStudents를 여기에 렌더링하세요. */}
+        {
+          /* TODO: map을 사용해서 filteredStudents를 여기에 렌더링하세요. */
+          filteredStudents.map(
+            <li key={students}>
+              {students.name} {students.age}
+            </li>
+          )
+        }
         {/* TODO: 학생이름을 클릭하면 나이와 점수가 alert 돼야 해요.*/}
       </ul>
     </div>
