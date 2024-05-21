e88a665a77af118b6776ffc23723ea12c1a2a72a
요니'^ <jjy606@naver.com>
2024-05-21 11:54:36 +0900

    ex3제출

--- src/App.jsx
+++ src/App.jsx
@@ -18,18 +18,14 @@ function App() {
   const [filteredStudents, setFilteredStudents] = useState(students);
 
   // TODO: filterByAge 함수를 작성하세요. 이 함수는 최소 나이를 매개변수로 받아 해당 나이 이상인 학생들로 필터링해야 합니다.
-  const filterByAge = (minAge) => {
-    // 여기에 코드를 작성하세요.
-  };
+  const filterByAge = (minAge) => {};
 
   // TODO: filterByGrade 함수를 작성하세요. 이 함수는 특정 학점을 매개변수로 받아 해당 학점의 학생들로 필터링해야 합니다.
-  const filterByGrade = (grade) => {
-    // 여기에 코드를 작성하세요.
-  };
+  const filterByGrade = (grade) => {};
 
   // TODO: resetFilter 함수를 작성하세요. 이 함수는 필터를 초기화하여 모든 학생들을 표시해야 합니다.
   const resetFilter = () => {
-    // 여기에 코드를 작성하세요.
+    setFilteredStudents(null);
   };
 
   return (
